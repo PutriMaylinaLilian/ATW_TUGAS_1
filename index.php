eb6d51b1f5b6fc7969a387da90228defbbac6678
PutriMaylinaLilian <71164747+PutriMaylinaLilian@users.noreply.github.com>
2020-09-12 12:41:05 -0700

    Penambahan Index serta Class

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+
+echo "Animal <br>";
+echo "<hr>";
+echo "<hr>";
+
+class Kucing {
+	public $Nama, $Jumlah_Kaki, $Bisa_Terbang, $Suara;
+}
+
+$Kucing = new Kucing;
+$Kucing->Nama = "Cola";
+$Kucing->Jumlah_Kaki = "4";
+$Kucing->Bisa_Terbang = "Tidak";
+$Kucing->Suara = "Meow Meow Meow";
+
+echo "Kucing <br>";
+echo "Nama         : $Kucing->Nama <br>";
+echo "Jumlah Kaki  : $Kucing->Jumlah_Kaki <br>";
+echo "Bisa Terbang : $Kucing->Bisa_Terbang <br>";
+echo "Suaranya     : $Kucing->Suara <br>";
+echo "<hr>";
+echo "<hr>";
+
+
+class Anjing{
+	public $Nama, $Jumlah_Kaki, $Bisa_Terbang, $Suara;
+}
+
+$Anjing = new Anjing;
+$Anjing->Nama = "Haruto";
+$Anjing->Jumlah_Kaki = "4";
+$Anjing->Bisa_Terbang = "Tidak";
+$Anjing->Suara = "Guk Guk Guk";
+
+echo "Anjing <br>";
+echo "Nama         : $Anjing->Nama <br>";
+echo "Jumlah Kaki  : $Anjing->Jumlah_Kaki <br>";
+echo "Bisa Terbang : $Anjing->Bisa_Terbang <br>";
+echo "Suaranya     : $Anjing->Suara <br>";
+echo "<hr>";
+echo "<hr>";
+
+
+class Elang{
+	public $Nama, $Jumlah_Kaki, $Bisa_Terbang, $Suara;
+}
+
+$Elang = new Elang;
+$Elang->Nama = "Lucas";
+$Elang->Jumlah_Kaki = "2";
+$Elang->Bisa_Terbang = "Ya";
+$Elang->Suara = "Miiip Miiip Miiip";
+
+echo "Elang <br>";
+echo "Nama         : $Elang->Nama <br>";
+echo "Jumlah Kaki  : $Elang->Jumlah_Kaki <br>";
+echo "Bisa Terbang : $Elang->Bisa_Terbang <br>";
+echo "Suaranya     : $Elang->Suara <br>";
+echo "<hr>";
+echo "<hr>";
+
+
+class Angsa{
+	public $Nama, $Jumlah_Kaki, $Bisa_Terbang, $Suara;
+}
+
+$Angsa = new Angsa;
+$Angsa->Nama = "Mark Lee";
+$Angsa->Jumlah_Kaki = "2";
+$Angsa->Bisa_Terbang = "Ya";
+$Angsa->Suara = "Kwakkk Kwakkk Kwakkk";
+
+echo "Angsa <br>";
+echo "Nama         : $Angsa->Nama <br>";
+echo "Jumlah Kaki  : $Angsa->Jumlah_Kaki <br>";
+echo "Bisa Terbang : $Angsa->Bisa_Terbang <br>";
+echo "Suaranya     : $Angsa->Suara <br>";
+echo "<hr>";
+echo "<hr>";
+
